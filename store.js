572dcf0552bb8cf27cae279bd78efa67b34bbab1
jiurenci <110651433+jiurenci@users.noreply.github.com>
2022-08-05 19:31:49 +0800

    Update store.js

--- store.js
+++ store.js
@@ -251,7 +251,7 @@ window.onlineShop = {
 	    unique:false,/*识别码*/
             title:'七夕礼包',
 	    info:'元宝*999、刘备*1、孙尚香*1、寻宝令*10、招募令、10、烽火连天',
-	    pack:[{id:'coupons',num:999},{id:'xunbaoling',num:10},{id:'zhaomuling',num:10},{id:'liubei'},{id:'mayunlu'},{id:'xushao',num:'烽火连天.jpg'}],/*打包*/
+	    pack:[{id:'coupons',num:999},{id:'xunbaoling',num:10},{id:'zhaomuling',num:10},{id:'liubei'},{id:'sunshangxiang'},{id:'xushao',num:'烽火连天.jpg'}],/*打包*/
 	    ext:'1',
         },
     ],
